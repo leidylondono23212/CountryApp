c70072f470c25d7dd162431973e8cf176adb64f0
Leidy Londoño <leidy_londono23212@elpoli.edu.co>
2023-08-05 19:00:06 -0500

    Fin de sección 9

--- src/app/countries/services/countries.service.ts
+++ src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable,catchError,of } from 'rxjs';
+import { Observable,catchError,map,of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({providedIn: 'root'})
@@ -9,6 +9,16 @@ export class CountriesService {
   private apiUrl : string = 'https://restcountries.com/v3.1'
 
   constructor(private http: HttpClient) { }
+
+  searchCountryByAlphaCode(code: string):Observable <Country | null>{
+    const url = `${this.apiUrl}/alpha/${code}`
+    return this.http.get<Country[]>(url)
+      .pipe(
+        map(countries => countries.length > 0 ? countries[0]: null),
+        catchError(error => of(null))
+      );
+
+  }
 
 
   searchCapital(term:string):Observable <Country[]>{
@@ -20,13 +30,24 @@ export class CountriesService {
 
   }
 
-  // searchCountry(term:string):Observable <Country[]>{
+  searchCountry(term:string):Observable <Country[]>{
+    const url = `${this.apiUrl}/name/${term}`
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([]))
+      );
 
-  // }
+
+  }
 
 
-  // searchRegion(region:string):Observable <Country[]>{
+  searchRegion(region:string):Observable <Country[]>{
+    const url = `${this.apiUrl}/region/${region}`
+    return this.http.get<Country[]>(url)
+      .pipe(
+        catchError(error => of([]))
+      );
 
-  // }
+  }
 
 }
